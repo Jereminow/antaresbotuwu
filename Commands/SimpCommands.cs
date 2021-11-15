9a82ec5f0c825e827097c6a52ed2fed14e3d6a17
Jereminow <Jerenowi@gmail.com>
2021-11-15 15:51:54 +0100

    [TEST] Help me

--- Commands/SimpCommands.cs
+++ Commands/SimpCommands.cs
@@ -131,11 +131,11 @@ namespace Antares_bot_uwu
                 {
                     html = reader.ReadToEnd();
                 }
-                string[] temp_split = html.Split("url\":\"");
-                string[] temp_split2 = temp_split[0].Split("\"");
+                string[] temp_split = html.Split("url");
+                //string[] temp_split2 = temp_split[0].Split("\"");
 
                 //await ReplyAsync(html.Substring(0, 100));
-                await ReplyAsync(temp_split[0].Substring(0,200));
+                await ReplyAsync(temp_split[1].Substring(0,200));
             }
             catch (Exception ex) { await ReplyAsync(ex.Message); }
 
